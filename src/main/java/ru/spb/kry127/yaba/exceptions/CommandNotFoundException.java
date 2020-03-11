573e23553bd118ad753cad7be82ed156bee4e6b4
Kry127 <kry127@yandex.ru>
2020-03-11 04:49:06 +0300

    Added grep

--- src/main/java/ru/spb/kry127/yaba/exceptions/CommandNotFoundException.java
+++ src/main/java/ru/spb/kry127/yaba/exceptions/CommandNotFoundException.java
@@ -9,14 +9,18 @@ public class CommandNotFoundException extends Exception {
 
     private final static String OUTPUT_PATTERN = "Cannot find command {0} :(";
 
-    /**
-     * У этой ошибки, скорее всего, не может быть причины.
-     * Можно добавить второй конструктор, принимающий Throwable
-     * по мере надобности
-     *
-     * @param expr строка, которую следует подставить в шаблон
-     */
-    public CommandNotFoundException(String expr) {
-        super(MessageFormat.format(OUTPUT_PATTERN, expr));
-    }
+  /**
+   * У этой ошибки, скорее всего, не может быть причины.
+   * Можно добавить второй конструктор, принимающий Throwable
+   * по мере надобности
+   *
+   * @param expr строка, которую следует подставить в шаблон
+   */
+  public CommandNotFoundException(String expr) {
+    super(MessageFormat.format(OUTPUT_PATTERN, expr));
+  }
+
+  public CommandNotFoundException(String expr, Throwable cause) {
+    super(MessageFormat.format(OUTPUT_PATTERN, expr), cause);
+  }
 }
